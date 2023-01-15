94433d953ecd7475e59817361ba2c28c8217cde0
folkengine <gaoler@electronicpanopticon.com>
2023-01-15 13:58:27 -0800

    ...

--- src/play/board.rs
+++ src/play/board.rs
@@ -259,7 +259,7 @@ mod play_board_tests {
     fn river__2_players() {
         let board = Board::from_str("A♠ K♠ 2♣ 3♣ T♦").unwrap();
 
-        let win = board.river(&vec![Two::HAND_JC_4H, Two::HAND_8C_7C]);
+        let win = board.river(&git statusgitadsddvec![Two::HAND_JC_4H, Two::HAND_8C_7C]);
 
         assert_eq!(Win::FIRST, win);
     }
