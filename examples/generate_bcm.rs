54ce423e5b838d121dddddf69bc0e1f5781961d0
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2023-07-10 06:32:46 -0700

    comments

--- examples/generate_bcm.rs
+++ examples/generate_bcm.rs
@@ -1,6 +1,11 @@
 use pkcore::analysis::store::bcm::binary_card_map::BinaryCardMap;
 
-/// cargo run --example generate_bcm
+/// RUST_LOG=trace cargo run --example generate_bcm
 fn main() {
+    let now = std::time::Instant::now();
+    env_logger::init();
+
     BinaryCardMap::generate_csv("generated/bcm.csv").expect("TODO: panic message");
+
+    println!("Elapsed: {:.2?}", now.elapsed());
 }
