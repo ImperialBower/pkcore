7f4a63cd8d6dc2c14ac702ff7ae415abfddd7824
folkengine <gaoler@electronicpanopticon.com>
2023-11-13 17:26:00 -0500

    working through is_open()

--- src/play/action.rs
+++ src/play/action.rs
@@ -12,6 +12,7 @@ pub enum PlayState {
     Bet,
     Call,
     Raise,
+    Fubar,
 }
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
@@ -32,13 +33,29 @@ pub struct Action {
 }
 
 impl Action {
+    /// # Panics
+    ///
+    /// Throws a panic if `count` is less than 2.
     #[must_use]
     pub fn new(count: usize) -> Self {
         let mut actors = Vec::with_capacity(count);
         for _ in 0..count {
             actors.push(Actor::default());
         }
+        actors.first().unwrap().state.set(PlayState::SmallBlind);
+        actors.get(1).unwrap().state.set(PlayState::BigBlind);
         Action { actors }
+    }
+
+    #[must_use]
+    pub fn is_open(&self) -> bool {
+        if self.actors.len() < 2 {
+            return false;
+        }
+
+        self.actors
+            .iter()
+            .any(|a| a.state.get() == PlayState::YetToAct)
     }
 
     #[must_use]
@@ -55,6 +72,9 @@ impl Action {
 
     #[must_use]
     pub fn state(&self, position: usize) -> PlayState {
+        if position > self.actors.len() - 1 {
+            return PlayState::Fubar;
+        }
         self.actors[position].state.get()
     }
 }
@@ -68,10 +88,43 @@ mod play__action__tests {
     fn new() {
         let actions = Action::new(6);
 
-        assert_eq!(PlayState::YetToAct, actions.state(0));
-        assert_eq!(PlayState::YetToAct, actions.state(1));
+        assert_eq!(PlayState::SmallBlind, actions.state(0));
+        assert_eq!(PlayState::BigBlind, actions.state(1));
         assert_eq!(PlayState::YetToAct, actions.state(2));
+        assert_eq!(PlayState::YetToAct, actions.state(3));
+        assert_eq!(PlayState::YetToAct, actions.state(4));
+        assert_eq!(PlayState::YetToAct, actions.state(5));
     }
+
+    #[test]
+    fn is_open__any_yet_to_act() {
+        let actions = Action::new(6);
+        assert!(actions.is_open());
+
+        actions.set_state(0, PlayState::Call);
+        assert!(actions.is_open());
+
+        actions.set_state(1, PlayState::Fold);
+        assert!(actions.is_open());
+
+        actions.set_state(2, PlayState::Fold);
+        assert!(actions.is_open());
+
+        actions.set_state(3, PlayState::Fold);
+        assert!(actions.is_open());
+
+        actions.set_state(4, PlayState::Fold);
+        assert!(actions.is_open());
+
+        actions.set_state(5, PlayState::Fold);
+        assert!(!actions.is_open());
+    }
+
+    // #[test]
+    // fn is_open__only_one_player_let_standing() {
+    //     let actions = Action::new(6);
+    //
+    // }
 
     #[test]
     fn player_count() {
@@ -80,5 +133,12 @@ mod play__action__tests {
 
         actions.set_state(0, PlayState::Fold);
         assert_eq!(5, actions.player_count());
+    }
+
+    #[test]
+    fn state_invalid_index() {
+        let actions = Action::new(6);
+
+        assert_eq!(PlayState::Fubar, actions.state(6));
     }
 }
