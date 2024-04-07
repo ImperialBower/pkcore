8f7cff8cfe1befbf6795c007092e279ba495c93f
folkengine <gaoler@electronicpanopticon.com>
2024-04-07 06:41:14 -0700

    ...

--- src/games/omaha.rs
+++ src/games/omaha.rs
@@ -10,6 +10,7 @@ use crate::PKError;
 use std::fmt;
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
+use crate::card::Card;
 
 pub const OMAHA_HAND_PERMUTATIONS: [[usize; 2]; 6] = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]];
 pub const OMAHA_BOARD_PERMUTATIONS: [[usize; 3]; 10] = [
@@ -25,19 +26,20 @@ pub const OMAHA_BOARD_PERMUTATIONS: [[usize; 3]; 10] = [
     [2, 3, 4],
 ];
 
+const PERMUTATIONS: bint::Bint = bint::Bint{ value: 0, boundary: 30};
+
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct OmahaHigh {
     pub hand: Four,
 }
 
-/// The hand:
-/// Robl: AS QS QD JC
-/// Antonius: 9H 8D 6D 5D
-/// board: 4D AD 7S JD AC
-/// https://www.youtube.com/watch?v=iXmrtiqoUKM
 impl OmahaHigh {
+    fn perm_indexes(index: usize) -> (usize, usize) {
+        todo!()
+    }
+
     #[must_use]
-    pub fn permutation(&self, board: &Five, from_hand: usize, from_board: usize) -> Seven {
+    pub fn permutation(&self, board: &Five, from_hand: usize, from_board: usize) -> Five {
         todo!()
     }
 
@@ -65,6 +67,18 @@ impl Display for OmahaHigh {
     }
 }
 
+impl From<Four> for OmahaHigh {
+    fn from(four: Four) -> Self {
+        OmahaHigh { hand: four }
+    }
+}
+
+impl From<[Card; 4]> for OmahaHigh {
+    fn from(array: [Card; 4]) -> Self {
+        OmahaHigh { hand: Four::from(array) }
+    }
+}
+
 impl FromStr for OmahaHigh {
     type Err = PKError;
 
@@ -86,24 +100,47 @@ impl TryFrom<Cards> for OmahaHigh {
 #[allow(non_snake_case)]
 mod games__omaha_high_tests {
     use super::*;
-    use crate::card::Card;
-    use std::str::FromStr;
+
+    /// The hand:
+    /// Robl: AS QS QD JC
+    /// Antonius: 9H 8D 6D 5D
+    /// board: 4D AD 7S JD AC
+    /// https://www.youtube.com/watch?v=iXmrtiqoUKM
+    const ROBL_HAND: [Card; 4] = [
+        Card::ACE_SPADES,
+        Card::QUEEN_SPADES,
+        Card::QUEEN_DIAMONDS,
+        Card::JACK_CLUBS,
+    ];
+
+    const ANTONIUS_HAND: [Card; 4] = [
+        Card::NINE_HEARTS,
+        Card::EIGHT_DIAMONDS,
+        Card::SIX_DIAMONDS,
+        Card::FIVE_DIAMONDS,
+    ];
 
     #[test]
     fn display() {}
 
     #[test]
-    fn from_str() {
+    fn from_four() {
         let expected = OmahaHigh {
-            hand: Four::from([
-                Card::ACE_SPADES,
-                Card::QUEEN_SPADES,
-                Card::QUEEN_DIAMONDS,
-                Card::JACK_CLUBS,
-            ]),
+            hand: Four::from(ROBL_HAND)
         };
 
-        let actual = OmahaHigh::from_str("AS QS QD JC").unwrap();
+        let actual = OmahaHigh::from(expected.hand);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn from_str() {
+        let expected = OmahaHigh {
+            hand: Four::from(ANTONIUS_HAND),
+        };
+
+        let actual = OmahaHigh::from_str("9H 8D 6D 5D").unwrap();
 
         assert_eq!(expected, actual);
     }
