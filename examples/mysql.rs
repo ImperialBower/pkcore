0674fcbb130df5676a70f05424b51d676e18471a
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2024-07-20 17:25:33 -0700

    more dump

--- examples/mysql.rs
+++ examples/mysql.rs
@@ -1,10 +1,20 @@
 use mysql::*;
-use pkcore::analysis::store::db::mysql::DbConnectOps;
+use pkcore::analysis::store::db::mysql::{DbConnectOps, HeadsUpRawResult};
 use pkcore::analysis::store::db::mysql::MySqlDB;
+use pkcore::util::csv::distinct_shus_from_csv_as_masked_vec;
 
 /// `cargo run --example mysql`
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
     println!("MySQL version: {:?}", MySqlDB::version_string().unwrap());
+
+    let mut conn = MySqlDB::get_connection()?;
+    let distinct = distinct_shus_from_csv_as_masked_vec().len();
+    let hups = HeadsUpRawResult::all(&mut conn).unwrap().len();
+    let remaining = distinct - hups;
+
+    println!("{hups} done {remaining} hands remaining");
 
     Ok(())
 }
