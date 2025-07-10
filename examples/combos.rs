55d695c533e6a0d24f90beb37779d0e9d24e31c0
folkengine <gaoler@electronicpanopticon.com>
2025-07-10 12:13:25 -0700

    working on razz hands

--- examples/combos.rs
+++ examples/combos.rs
@@ -1,7 +1,24 @@
+use pkcore::arrays::five::Five;
+use pkcore::arrays::HandRanker;
 use pkcore::arrays::two::Two;
 use pkcore::deck::POKER_DECK;
 
 fn main() {
+
+
+}
+
+fn hands() -> Vec<Five> {
+    let combos = POKER_DECK.combinations(5);
+
+    let hands: Vec<Five> = combos.map(|c| {
+        Five::try_from(c).unwrap().sort()
+    }).collect();
+
+    hands
+}
+
+fn _twos() {
     let combos = POKER_DECK.combinations(2);
 
     let twos: Vec<Two> = combos.map(|c| Two::from(c)).collect();
