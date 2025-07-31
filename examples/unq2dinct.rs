18a99d5c462c74bc4b80d494d89b47aa4668e8f8
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2025-07-31 13:39:02 -0700

    updated with 1 hand unq

--- examples/unq2dinct.rs
+++ examples/unq2dinct.rs
@@ -2,7 +2,8 @@ use rusqlite::Connection;
 use pkcore::analysis::store::db::headsup_preflop_result::HUPResult;
 use pkcore::analysis::store::db::sqlite::Sqlable;
 use pkcore::arrays::matchups::sorted_heads_up::SortedHeadsUp;
-use pkcore::PKError;
+use pkcore::arrays::two::Two;
+use pkcore::{PKError, Shifty};
 
 // `cargo run --example unq2dinct`
 fn main() -> Result<(), PKError> {
@@ -13,6 +14,17 @@ fn main() -> Result<(), PKError> {
     };
     let hups = HUPResult::select_all(&conn);
     println!("{} shus processed", hups.len());
+
+    let hero = Two::HAND_AS_AH;
+    let villain = Two::HAND_AD_AC;
+
+    let mut shu = SortedHeadsUp::new(hero, villain);
+
+    let shifts = shu.shifts();
+
+    for shift in shifts {
+        println!("{shift}");
+    }
 
     Ok(())
 }
