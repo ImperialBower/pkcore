e53e5900dd1fda769229be560623bf17bab9aa21
folkengine <gaoler@electronicpanopticon.com>
2025-12-03 18:46:47 -0800

    Adjusted Player.bet to be additive

--- src/casino/player.rs
+++ src/casino/player.rs
@@ -41,6 +41,37 @@ impl Player {
     /// your natural instict to make everything mutable on its head. When I first coded this
     /// I made everything mutable even though I was working with a `Cell`.
     ///
+    /// **UPDATE:** The original version of this code simply removed chips from the player's stack,
+    /// and placed them in the best stack, erroring out of there weren't enough chips for the bet.
+    ///
+    /// I've updated it to reflect how play works in reality. When a player bets, and then raises
+    /// in a round, they announce the total amount they are betting. So if they bet 50, and then
+    /// raise to 100, they are only putting in an additional 50 chips, not 100 more.
+    ///
+    /// Now, the function, subtracts the amount already bet from the bet amount, and processes
+    /// the bet based on the total chips committed to the pot for that round.
+    ///
+    /// NOTE: At the
+    ///
+    /// ```
+    /// use pkcore::prelude::*;
+    ///
+    /// let player = Player::new_with_chips("The Russian".to_string(), 1_000);
+    ///
+    /// let first_bet = player.bets(50);
+    /// let second_bet = player.bets(100);
+    /// let third_bet = player.bets(100);
+    ///
+    /// assert!(first_bet.is_ok());
+    /// assert!(second_bet.is_ok());
+    /// assert!(third_bet.is_err());
+    /// assert_eq!(PKError::InsufficientChips, third_bet.unwrap_err());
+    /// assert_eq!(950, first_bet.unwrap());
+    /// assert_eq!(900, second_bet.unwrap());
+    /// assert_eq!(100, player.bet.count());
+    /// assert_eq!(900, player.chips.count());
+    /// ```
+    ///
     /// # Errors
     ///
     /// * `PKError::InsufficientChips` - if the player does not have enough chips to make the bet
@@ -48,7 +79,16 @@ impl Player {
         if amount > self.chips.count() {
             Err(PKError::InsufficientChips)
         } else {
-            let bet_chips = self.chips.bet(amount)?;
+            // How many chips are there above what's already committed to the round?
+            let additional_bet = amount.saturating_sub(self.bet.count());
+
+            // Throw an error if the result is 0, meaning they aren't betting anything.
+            if additional_bet == 0 {
+                log::warn!("InsufficientChips: Bet amount already placed.");
+                return Err(PKError::InsufficientChips);
+            }
+
+            let bet_chips = self.chips.bet(additional_bet)?;
             self.state.set(PlayerState::Bet(bet_chips.count()));
             self.bet.add_to(bet_chips);
             Ok(self.chips.count())
