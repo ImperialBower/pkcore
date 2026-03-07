4c02dcd45f43df525ff2be968a8575b1cce0f7bc
Christoph Baker <ignatz@gmail.com>
2026-03-07 10:11:06 -0800

    make Makefile docs command linux compatible

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+#[allow(clippy::unnecessary_wraps)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Only compile proto files if tonic-build is available
     // This allows the project to build without gRPC dependencies
