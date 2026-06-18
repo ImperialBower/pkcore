d96c4c9371607bedce2d1ece4fda81e8e8e2d29e
folkengine <gaoler@electronicpanopticon.com>
2026-06-17 19:45:56 -0700

    bench: add preflop_odds criterion baseline (heads-up brute force ~6.4s)

---
+++ benches/preflop_odds.rs
@@ -0,0 +1,19 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use pkcore::prelude::{DealEval, HoleCards, Two};
+
+fn heads_up(c: &mut Criterion) {
+    let hands = HoleCards::from(vec![Two::HAND_AS_AH, Two::HAND_KS_KH]);
+    c.bench_function("deal eval heads up", |b| {
+        b.iter(|| DealEval::new(black_box(hands.clone())));
+    });
+}
+
+fn three_way(c: &mut Criterion) {
+    let hands = HoleCards::from(vec![Two::HAND_AS_AH, Two::HAND_KS_KH, Two::HAND_2D_2C]);
+    c.bench_function("deal eval three way", |b| {
+        b.iter(|| DealEval::new(black_box(hands.clone())));
+    });
+}
+
+criterion_group!(benches, heads_up, three_way);
+criterion_main!(benches);
