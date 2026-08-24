c08f4102e9d92c524c9185b2326e919ce870e167
folkengine <gaoler@electronicpanopticon.com>
2026-08-23 20:02:45 -0400

    added spike-kernel

---
+++ docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a.d
@@ -0,0 +1,5 @@
+/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a.d: tests/kernel_tests.rs
+
+/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a: tests/kernel_tests.rs
+
+tests/kernel_tests.rs:
---
+++ docs/epics/EPIC-82_spike-kernel/target/debug/deps/spike_kernel-a1d0ff89a6c6c549.d
@@ -0,0 +1,9 @@
+/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/spike_kernel-a1d0ff89a6c6c549.d: src/lib.rs src/kernel.rs src/shells.rs
+
+/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/libspike_kernel-a1d0ff89a6c6c549.rlib: src/lib.rs src/kernel.rs src/shells.rs
+
+/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/libspike_kernel-a1d0ff89a6c6c549.rmeta: src/lib.rs src/kernel.rs src/shells.rs
+
+src/lib.rs:
+src/kernel.rs:
+src/shells.rs:
