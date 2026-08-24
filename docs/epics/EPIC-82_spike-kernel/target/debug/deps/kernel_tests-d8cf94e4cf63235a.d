bde2353f28ce2c9dfb6b5b5b8549de7892387ac6
folkengine <gaoler@electronicpanopticon.com>
2026-08-23 20:02:55 -0400

    added spike-kernel

--- docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a.d
+++
@@ -1,5 +0,0 @@
-/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a.d: tests/kernel_tests.rs
-
-/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/kernel_tests-d8cf94e4cf63235a: tests/kernel_tests.rs
-
-tests/kernel_tests.rs:
--- docs/epics/EPIC-82_spike-kernel/target/debug/deps/spike_kernel-9a2025a7bb262662.d
+++
@@ -1,7 +0,0 @@
-/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/spike_kernel-9a2025a7bb262662.d: src/lib.rs src/kernel.rs src/shells.rs
-
-/Users/christoph/src/github.com/ImperialBower/pkcore/docs/epics/EPIC-82_spike-kernel/target/debug/deps/spike_kernel-9a2025a7bb262662: src/lib.rs src/kernel.rs src/shells.rs
-
-src/lib.rs:
-src/kernel.rs:
-src/shells.rs:
